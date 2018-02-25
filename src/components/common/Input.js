3e63643d7fe38ee1487bf614a581dff1b61826e0
Vinícius Silva <vinicius768@msn.com>
2018-02-25 00:02:38 -0300

    Added input basic style as const

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { Text, TextInput, View } from 'react-native';
 
 const Input = ({ label }) => {
+    const { inputStyle } = styles;
+
     return (
         <View>
             <Text>{label}</Text>
-            
+
             <TextInput
                 value={this.state.text}
                 onChangeText={text => this.setState({ text })}
-                style={{ height: 40, width: 100 }}
+                style={inputStyle}
             />
         </View>
     );
+};
+
+const styles = {
+    inputStyle: {
+        height: 40,
+        width: 100
+    }
 };
 
 export { Input };
