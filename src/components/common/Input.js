1ce336d570bb8aa57258e921f8cc2dc73ccde027
Vinícius Silva <vinicius768@msn.com>
2018-02-25 00:18:37 -0300

    Added styling to input

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Text, TextInput, View } from 'react-native';
 
 const Input = ({ label, onChangeText, value }) => {
-    const { inputStyle } = styles;
+    const { containerStyle, inputStyle, labelStyle } = styles;
 
     return (
-        <View>
-            <Text>{label}</Text>
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
 
             <TextInput
                 value={value}
@@ -18,9 +18,24 @@ const Input = ({ label, onChangeText, value }) => {
 };
 
 const styles = {
-    inputStyle: {
+    containerStyle: {
         height: 40,
-        width: 100
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    inputStyle: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
     }
 };
 
